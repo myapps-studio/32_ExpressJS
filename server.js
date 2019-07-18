9c9a7a3b8102e29a4589b62251064bc1d46b21ed
Andrzej Mieczkowski <praca.mieczkowski@gmail.com>
2019-07-18 15:28:27 +0200

    finished

--- server.js
+++ server.js
@@ -15,22 +15,22 @@ app.get('/getNote', function (req, res) {
 
     fs.readFile('./test.json', 'utf8', function(err, data) {
         if (err) throw err;
-        stringifyFile = data
+        stringifyFile = data;
         res.send(data);
     });
 });
 
 app.post('/updateNote/:note', function (req, res) {
     console.log('Otrzymałem żądanie POST do strony głównej');
-    
-    stringifyFile = req.params.note;
+
+    stringifyFile = stringifyFile + req.params.note;
 
     fs.writeFile('./test.json', stringifyFile, function(err) {
-        if (err) throw err;
+        if (err) throw err; 
         console.log('file updated');
     });
 
-    //res.send('Hello POST!');
+    res.send('Hello POST!');
 });
 
 app.listen(3000);
